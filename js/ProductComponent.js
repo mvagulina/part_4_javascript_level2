de9c22934add9dda21312234f4c5e2a7684edab5
mvagulina <marinka_2001@mail.ru>
2020-03-24 17:41:59 +0300

    products component

--- js/ProductComponent.js
+++ js/ProductComponent.js
@@ -1,17 +1,18 @@
 Vue.component('products', {
     props: ['products'],
     template: `<div class="products-list">
-               <div class="product" v-for="product of products" :key="product.id">
+                   <product :product="product" v-for="product of products" :key="product.id"></product>
+               </div>`
+});
+
+Vue.component('product', {
+    props: ['product'],
+    template: `<div class="product">
                    <img v-bind:src="'img/catalog/product-'+ product.id + '.jpg'">
                    <div>
                        <h3>{{product.name}}</h3>
                         <span>{{product.price}} $</span>
                         <button class="buy-btn" @click="addProduct(product)">Купить</button>
                    </div>
-               </div>
-    `
-});
-
-Vue.component('product', {
-    
+               </div>`
 });
